5e4acb3c73a87a42d28d8e481376fab97fc121f9
peiyouyao <peiyouyao@gmail.com>
2025-06-23 20:25:19 +0800

    kitchen

---
+++ internal/kitchen/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/peiyouyao/gorder/common/broker"
+	grpcClient "github.com/peiyouyao/gorder/common/client"
+	_ "github.com/peiyouyao/gorder/common/config"
+	"github.com/peiyouyao/gorder/common/logging"
+	"github.com/peiyouyao/gorder/common/tracing"
+	"github.com/peiyouyao/gorder/kitchen/adapters"
+	"github.com/peiyouyao/gorder/kitchen/infrastructure/consumer"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	logging.Init()
+}
+
+func main() {
+	serviceName := viper.GetString("kitchen.service-name")
+
+	ctx, cancal := context.WithCancel(context.Background())
+	defer cancal()
+
+	shutdown, err := tracing.InitJaegerProvider(viper.GetString("jaeger.url"), serviceName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer shutdown(ctx)
+
+	orderClient, closeFn, err := grpcClient.NewOrderGRPCClient(ctx)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer closeFn()
+
+	ch, closeCh := broker.Connect(
+		viper.GetString("rabbitmq.user"),
+		viper.GetString("rabbitmq.password"),
+		viper.GetString("rabbitmq.host"),
+		viper.GetString("rabbitmq.port"),
+	)
+	defer func() {
+		_ = ch.Close()
+		_ = closeCh()
+	}()
+
+	orderGRPC := adapters.NewOrderGRPC(orderClient)
+	go consumer.NewConsumer(orderGRPC).Listen(ch)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-sigs
+		logrus.Info("receive signal, exiting ...")
+		os.Exit(0)
+	}()
+	logrus.Println("to exit, press ctrl + c")
+	select {}
+}
